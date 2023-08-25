09a0a9068df6b287e6285708a569215227b9df66
Yash-Patel-42 <121545728+Yash-Patel-42@users.noreply.github.com>
2023-08-25 12:40:45 +0530

    Delete Yash directory

--- Yash/code.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	int a;
-	printf("Enter a Number:");
-	scanf("%d",&a);
-	printf("The Number is:",a);
-}
--- Yash/code.py
+++
@@ -1,3 +0,0 @@
-print("Enter a Number");
-a = input();
-print("The Entered Number is:"+a);
